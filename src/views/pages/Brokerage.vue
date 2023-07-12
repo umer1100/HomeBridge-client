ac29f4ab829e6398e4b137e24a79fd5ec5c696b2
Dev Shah <DevShah@users.noreply.github.com>
2023-07-12 14:44:28 -0400

    Update buy tab (#23)

--- src/views/pages/Brokerage.vue
+++ src/views/pages/Brokerage.vue
@@ -1,33 +1,36 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="mt-4 card col-12 col-lg-4 mx-1">
+      <div v-for="agent in agents" :key="agent.id" class="mt-4 card col-12 col-lg-4 mx-1 card-width">
         <div class="p-3 card-body">
           <div>
             <div class="d-flex">
               <div class="avatar avatar-lg">
-                <img alt="Image placeholder" src="https://ownerific-static-images.s3.amazonaws.com/Goodwin.png" />
+                <img v-bind:src="agent.image" />
               </div>
               <div class="my-auto ms-2">
-                <h6 class="mb-0">Marcus Goodwin</h6>
-                <span class="d-block text-sm">Keller Williams Capital Partners</span>
-                Buyer's Agent
+                <h6 class="mb-0">{{agent.name}}</h6>
+                <span class="d-block text-sm">{{ agent.description }}</span>
+                {{ agent.location }}
               </div>
             </div>
-            <!-- eslint-disable-next-line vue/no-v-html -->
-            <p class="mt-3">
-              Looking for the perfect home can be an exciting yet overwhelming process. That's why it's important to have a buyer's agent by your side who not only has the expertise to guide you through the market but also makes you feel welcomed every step of the way. My name is Marcus, a dedicated real estate professional who takes a friendly, buyer-first approach to help you find your dream home.
-            </p>
+            <p class="mt-3 mb-0"><span class="font-weight-bold">Phone: </span>{{ agent.phone }}</p>
+            <p class="my-0"><span class="font-weight-bold">Email: </span>{{ agent.email }}</p>
+            <p class="my-0"><span class="font-weight-bold">Licensed In: </span>{{ agent.licenseId }}</p>
           </div>
 
           <hr class="horizontal dark" />
 
           <div class="bottom-5 my-2 px-md py-md d-flex justify-content-between align-items-center">
-            <div>
-              ⭐️⭐️⭐️⭐️⭐️
-            </div>
-            <button v-if="roleType != 'EMPLOYER'" class="mb-0 btn btn-sm bg-gradient-success">
-              Redeem
+            <button v-if="roleType != 'EMPLOYER'"
+                    class="mb-1 btn btn-sm bg-gradient-info mx-2 btn-width"
+                    @click="changeUrl(agent.review)">
+              Reviews
+            </button>
+            <button v-if="roleType != 'EMPLOYER'"
+                    class="mb-1 btn btn-sm bg-gradient-success mx-2 btn-width"
+                    @click="changeUrl(agent.profile)">
+              Apply
             </button>
           </div>
         </div>
@@ -50,6 +53,64 @@ export default defineComponent({
 
     const roleType = ref(userStore.data?.roleType)
 
+    const agents = [
+      {
+        name: 'Keith James',
+        description: 'Coalition Properties, Keller Williams',
+        location: 'Washington, D.C.',
+        phone: '(202) 808-1381',
+        email: 'keith@coalitionpg.com',
+        licenseId: 'Washington, D.C., Maryland',
+        profile: 'https://www.coalitionpg.com/agent-profile/keith-james-103665236',
+        review: 'https://www.zillow.com/profile/keithjames70',
+        image: 'https://ownerific-static-images.s3.amazonaws.com/Keith+James+Headshot.png'
+      },
+      {
+        name: 'Margaret Babbington',
+        description: 'Mollaan Babbington Group, Compass',
+        location: 'Washington, D.C.',
+        phone: '(240) 460-4007',
+        email: 'mbabbington@compass.com',
+        licenseId: 'Washington, D.C., Virginia',
+        profile: 'https://mbgroupdmv.com/team/margaret-babbington',
+        review: 'https://www.zillow.com/profile/babbingtonteam',
+        image: 'https://ownerific-static-images.s3.amazonaws.com/Margaret+Babington+Headshot.png'
+      },
+      {
+        name: 'Yasemin Hocaoglu',
+        description: 'The Yas Team, Compass ',
+        location: 'Washington, D.C.',
+        phone: '(571) 206-2380',
+        email: 'yasemin.hocaoglu@compass.com',
+        licenseId: 'Washington, D.C., Virginia',
+        profile: 'https://www.theyasteam.com/yasemin-hocaoglu',
+        review: 'https://www.zillow.com/profile/yasemin%20hocaoglu',
+        image: 'https://ownerific-static-images.s3.amazonaws.com/Yasemin+Hocaoglu+Headshot.png'
+      },
+      {
+        name: 'Jon Lahey',
+        description: 'The Fine Living Group, eXp Realty',
+        location: 'Washington, D.C.',
+        phone: '(301) 651-4900',
+        email: 'jonlahey@thefinelivinggroup.com',
+        licenseId: 'Washington, D.C., Maryland, Virginia',
+        profile: 'https://www.thefinelivinggroup.com/agents/jon-lahey/',
+        review: 'https://www.zillow.com/profile/laheygroup',
+        image: 'https://ownerific-static-images.s3.amazonaws.com/Jon+Lahey+Headshot.png'
+      },
+      {
+        name: 'Dina Shaminova',
+        description: 'Artifact Homes',
+        location: 'Washington, D.C.',
+        phone: '(202) 640-8415',
+        email: 'dina@homesbyartifact.com',
+        licenseId: 'Washington, D.C., Maryland, Virginia',
+        profile: 'https://homesbyartifact.com/agents/dina-shaminova',
+        review: 'https://www.zillow.com/profile/Dina-Shaminova/',
+        image: 'https://ownerific-static-images.s3.amazonaws.com/Dina+Shaminova+Headshot.png'
+      }
+    ]
+
     onMounted(async ()=> {
       globalStore.state.showFooter = false
       globalStore.state.showNavs = true
@@ -58,6 +119,7 @@ export default defineComponent({
     return {
       roleType,
       PlaidLogo,
+      agents
     }
   }
 })
