1f75e30f7e7ad92da84d3a587a4bdd8f3d63eb8c
DevShah <bullet02@gmail.com>
2023-08-21 13:55:26 -0400

    Merge conflicts with main

--- src/router/routes.js
+++ src/router/routes.js
@@ -19,10 +19,7 @@ import Integrations from "../views/pages/Integrations.vue";
 import Brokerage from "../views/pages/Brokerage.vue";
 import Lender from "../views/pages/Lender.vue";
 import Resources from "../views/pages/Resources.vue";
-<<<<<<< HEAD
 import Close from "../views/pages/Close.vue"
-=======
->>>>>>> 1984edd531b50ffe988c34fb25869ba4ebfdbae4
 // import Kanban from "../views/applications/Kanban.vue";
 // import Wizard from "../views/applications/wizard/Wizard.vue";
 // import DataTables from "../views/applications/DataTables.vue";
